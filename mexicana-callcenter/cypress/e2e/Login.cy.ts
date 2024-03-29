bce6527ab4985cca194ba87fdcf0194162d17402
Santiago Benitez <A01782813@tec.mx>
2024-03-28 21:37:16 -0600

    fixed problems in the terminal

--- mexicana-callcenter/cypress/e2e/Login.cy.ts
+++ mexicana-callcenter/cypress/e2e/Login.cy.ts
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe('Login Tests', () => {
   beforeEach(() => {
     cy.visit('/'); 
--- mexicana-callcenter/src/components/CCPComponent.tsx
+++ mexicana-callcenter/src/components/CCPComponent.tsx
@@ -1,10 +1,11 @@
+// This component renders the CCP UI iframe into the website
+
 import { useEffect } from 'react';
 import "amazon-connect-streams"
 
-
 const CCPComponent = () => {
   useEffect(() => {
-    const containerDiv = document.getElementById("ccp-container");
+    const containerDiv = document.getElementById("ccp-container")!;
     const instanceURL = "https://mexicana-airline.my.connect.aws/ccp-v2/";
 
     const init = () => {
@@ -12,13 +13,13 @@ const CCPComponent = () => {
         ccpUrl: instanceURL,
         loginPopup: true,
         loginPopupAutoClose: true,
-        // loginOptions: {
-        //   autoClose: true,
-        //   height: 600,
-        //   width: 400,
-        //   top: 0,
-        //   left: 0
-        // },
+        loginOptions: {                 // optional, if provided opens login in new window
+          autoClose: true,              // optional, defaults to `false`
+          height: 600,                  // optional, defaults to 578
+          width: 400,                   // optional, defaults to 433
+          top: 0,                       // optional, defaults to 0
+          left: 0                       // optional, defaults to 0
+        },
         softphone: {
           allowFramedSoftphone: true,
           disableRingtone: false,
@@ -30,7 +31,6 @@ const CCPComponent = () => {
           enableVideoDeviceSettings: false,
           enablePhoneTypeSettings: true
         },
-        shouldAddNamespaceToLogs: false,
         ccpAckTimeout: 5000,
         ccpSynTimeout: 3000,
         ccpLoadTimeout: 10000
