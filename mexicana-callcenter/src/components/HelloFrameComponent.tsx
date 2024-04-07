3f8e2fe1105c01418d6dc7c76306a8ba15464f9f
fernicortlo <a01026613@tec.mx>
2024-04-06 21:49:47 -0600

    hello screen done

--- mexicana-callcenter/src/components/HelloFrameComponent.tsx
+++ mexicana-callcenter/src/components/HelloFrameComponent.tsx
@@ -12,36 +12,35 @@ const FrameComponent: FunctionComponent = () => {
   };
 
   return (
-    <div className="self-stretch flex flex-row items-start justify-center py-0 pr-5 pl-[21px] box-border max-w-full text-center text-primary font-paragraph">
-      <div className="w-[625px] flex flex-col items-start justify-start max-w-full">
-        <div className="self-stretch flex flex-row items-start justify-start py-0 pr-[30px] pl-[29px] box-border max-w-full">
-          <h1 
-            style={{ fontSize: '10rem' }} // Adjust the font-size as needed
-            className="m-0 flex-1 relative text-inherit font-bold font-inherit inline-block max-w-full"
-          >
-            Hello!
-          </h1>
-        </div>
-        <div className="self-stretch flex flex-row items-start justify-center gap-[25px] mq750:flex-wrap">
-          <button 
-            className="cursor-pointer [border:none] py-2.5 px-5 bg-primary flex-1 rounded-3xs flex items-center justify-center box-border w-full md:min-w-[195px] whitespace-nowrap hover:bg-slategray"
-            onClick={handleSignInClick}
-          >
-            <span className="text-lg font-paragraph text-tertiary text-center">
-              Sign in
-            </span>
-          </button>
-          <button 
-            className="cursor-pointer [border:none] py-2.5 px-5 bg-primary flex-1 rounded-3xs flex items-center justify-center box-border w-full md:min-w-[195px] whitespace-nowrap hover:bg-slategray"
-            onClick={handleSignUpClick}
-          >
-            <span className="text-lg font-paragraph text-tertiary text-center">
-              Sign up
-            </span>
-          </button>
-        </div>
+    <div className="frameComponent-container flex flex-row items-center justify-center py-0 box-border max-w-full text-center text-primary font-paragraph">
+    <div className="w-[625px] flex flex-col items-center justify-center max-w-full">
+      <div className="flex flex-row items-center justify-center py-0 px-[30px] box-border max-w-full">
+        <h1 
+          style={{ fontSize: '10rem' }} 
+          className="frameComponent-header m-0 relative text-inherit font-bold font-inherit inline-block max-w-full"
+        >
+          Hello!
+        </h1>
+      </div>
+
+      <div className="frameComponent-buttons self-stretch flex flex-row items-center justify-center">
+      <button 
+        className="frameComponent-button cursor-pointer py-2.5 px-5 flex-1 rounded-3xs flex items-center justify-center box-border w-full md:min-w-[195px] whitespace-nowrap"
+        onClick={handleSignInClick}
+      >
+        Sign in
+      </button>
+      <button 
+        className="frameComponent-button cursor-pointer py-2.5 px-5 flex-1 rounded-3xs flex items-center justify-center box-border w-full md:min-w-[195px] whitespace-nowrap"
+        onClick={handleSignUpClick}
+      >
+        Sign up
+      </button>
+
       </div>
     </div>
+  </div>
+
   );
 };
 
--- mexicana-callcenter/src/components/HelloFrameComponent1.tsx
+++ mexicana-callcenter/src/components/HelloFrameComponent1.tsx
@@ -2,16 +2,15 @@ import { FunctionComponent } from "react";
 
 const FrameComponent1: FunctionComponent = () => {
   return (
-    <header className="w-full flex items-center justify-between p-[15px] bg-tertiary shadow-md sticky top-0 z-50">
-        <img
-          className="object-contain" 
-          style={{ height: '50px' }} 
-          loading="lazy"
-          alt="Call Center Logo"
-          src="/callCenterLogo.png"
-        />
+    <header className="fixed top-0 w-full flex items-center justify-between p-[15px] bg-tertiary shadow-md">
+      <img
+        className="object-contain header-img" 
+        alt="Call Center Logo"
+        src="/callCenterLogo.png"
+      />
     </header>
   );
 };
 
 export default FrameComponent1;
+
