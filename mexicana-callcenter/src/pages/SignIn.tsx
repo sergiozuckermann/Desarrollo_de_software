525adc009cfecb6daa2ffe9310184d152df395af
A01366643 <80075513+A01366643@users.noreply.github.com>
2024-04-08 16:51:19 -0600

    Update SignIn.tsx
    Actualizaciones de Sign In

--- mexicana-callcenter/src/pages/SignIn.tsx
+++ mexicana-callcenter/src/pages/SignIn.tsx
@@ -30,7 +30,7 @@ const SignIn: FunctionComponent = () => {
         USERNAME: emailTextValue,
         PASSWORD: passwordTextValue,
       },
-      ClientId: "2gdenkjjd809jojhh7ojfqslf1", // required
+      ClientId: "7n1pkdlieo0jnsl5uht0vpd5pj", // required
     };
     const command = new InitiateAuthCommand(input);
 
@@ -66,76 +66,49 @@ const SignIn: FunctionComponent = () => {
   };
 
   return (
-    <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start gap-[35px] tracking-[normal] text-right text-41xl text-primary font-paragraph mq750:gap-[17px]">
+    <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start gap-[75px] tracking-[normal] text-right text-41xl text-primary font-paragraph sm:gap-[50px] md:gap-[30px]">
       <header className="self-stretch h-[100px] relative bg-tertiary shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] hidden" />
       <div className="w-[339px] h-[73px] relative font-medium hidden max-w-full">
         Sign In
       </div>
-      <div className="w-[269px] h-[75px] relative hidden text-center text-lg text-black">
-        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%]">
-          <img
-            className="absolute h-full w-[74.35%] top-[0px] right-[25.65%] bottom-[0px] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
-            alt=""
-            src="/image-12@2x.png"
-          />
-        </div>
-        <div className="absolute h-[calc(100%_-_16px)] w-[21.93%] top-[8px] right-[0%] bottom-[8px] left-[78.07%]">
-          <img
-            className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
-            alt=""
-            src="/mexicana-logoremovebgpreview-1@2x.png"
-          />
-          <img
-            className="absolute h-[57.63%] w-[77.8%] top-[7.63%] right-[17.29%] bottom-[34.75%] left-[4.92%] max-w-full overflow-hidden max-h-full object-cover"
-            alt=""
-            src="/5c54ffaa6210e5031ddf4b4320271804dibujosanimadosdeauriculares-1@2x.png"
-          />
-        </div>
-        <div className="absolute top-[10.67%] left-[12.64%]">Call Center</div>
+      <div className="self-stretch bg-tertiary shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] flex flex-row items-start justify-end pt-[15px] px-[41px] pb-3 box-border max-w-full z-[2]">
+        <div className="h-[100px] w-[1280px] relative bg-tertiary shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] hidden max-w-full" />
+        <h1 className="m-0 h-[73px] w-[339px] relative text-inherit font-medium font-inherit inline-block shrink-0 max-w-full mq450:text-[36px] mq1050:text-[48px]">
+          Sign In
+        </h1>
       </div>
-      <main className="self-stretch h-[697px] flex flex-row items-start justify-start pt-0 pb-[243px] pr-[107px] pl-[620px] box-border gap-[35px] max-w-full lg:pl-[310px] lg:pr-[53px] lg:box-border mq450:pl-5 mq450:pb-[103px] mq450:box-border mq750:gap-[17px] mq750:pl-[155px] mq750:pr-[26px] mq750:box-border mq1050:pb-[158px] mq1050:box-border">
-        <img
-          className="ml-[-829px] h-[732px] w-[794px] relative shrink-0 [debug_commit:1cbd860] max-w-[144%] z-[2]"
+      <main className="self-stretch h-[700px] flex flex-row items-start justify-start pt-0 pb-[250px] pr-[0px] pl-[700px] box-border gap-[35px] max-w-full lg:pl-[250px] lg:pr-[50px] lg:box-border mq40:pl-5 mq400:pb-[10px] mq400:box-border mq750:gap-[17px] mq750:pl-[155px] mq750:pr-[26px] mq750:box-border mq1050:pb-[158px] mq1050:box-border">
+      <img
+          className="ml-[-829px] h-[732px] w-[794px] relative shrink-0 [debug_commit:1cbd860] max-w-[144%] z-[2] sm:ml-0 lg:mr-0"
           alt=""
           src="/sign-in-label.svg"
         />
-
         <div className="self-stretch w-[553px] flex flex-col items-start justify-start pt-[114px] px-0 pb-0 box-border max-w-full mq450:pt-[74px] mq450:box-border">
           <form
             onSubmit={handleLogin}
-            className="m-0 self-stretch flex-1 flex flex-col items-end justify-start gap-[46px] shrink-0 [debug_commit:1cbd860] mq750:gap-[23px]"
+            className="m-0 self-stretch flex-1 flex flex-col items-end justify-start gap-[46px] shrink-0 [debug_commit:1cbd860] sm:gap-[30px] md:gap-[20px]"
           >
             <div className="self-stretch flex-1 flex flex-col items-start justify-start gap-[16.2px] max-w-full">
               <div className="w-[501px] flex-1 flex flex-row items-start justify-start relative max-w-full">
                 <div className="h-[697px] w-[632px] absolute my-0 mx-[!important] bottom-[-453px] left-[-620px]">
                   <div className="absolute top-[126px] left-[249px] w-[85px] h-[85px]">
-                    <img
-                      className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
-                      alt=""
-                      src="/mexicana-logoremovebgpreview-1-1@2x.png"
-                    />
-                    <img
-                      className="absolute h-[57.65%] w-[77.76%] top-[7.65%] right-[17.29%] bottom-[34.71%] left-[4.94%] max-w-full overflow-hidden max-h-full object-cover z-[1]"
-                      alt=""
-                      src="/5c54ffaa6210e5031ddf4b4320271804dibujosanimadosdeauriculares-1-1@2x.png"
-                    />
                   </div>
                   <img
                     className="absolute top-[0px] left-[-162px] w-[794px] h-[732px] z-[3]"
                     alt=""
-                    src="/vector-1.svg"
+                    src="/vector-2.svg"
                   />
                 </div>
                 <div className="self-stretch flex-1 flex flex-row items-start justify-start py-0 pr-0 pl-3 box-border max-w-full">
                   <img
                     className="h-[130px] flex-1 relative max-w-full overflow-hidden object-cover z-[4]"
-                    loading="lazy"
+                    loading="lazy" 
                     alt=""
-                    src="/callCenterLogo.png"
+                    src="/untitled-design-2-1@2x.png"
                   />
                 </div>
               </div>
-              <div className="self-stretch rounded-3xs bg-tertiary box-border flex flex-row items-start justify-start pt-4 px-[18px] pb-[10.800000000000182px] max-w-full border-[1px] border-solid border-marco">
+              <div className="self-stretch rounded-3xs bg-tertiary box-border flex flex-row items-start justify-start pt-4 px-[10px] pb-[10px] max-w-full border-[1px] border-solid border-marco mr-0">
                 <div className="h-[44.8px] w-[553px] relative rounded-3xs bg-tertiary box-border hidden max-w-full border-[1px] border-solid border-marco" />
                 <input
                   className="[border:none] [outline:none] font-paragraph text-lg bg-[transparent] h-[18px] w-[100%] relative text-marco text-left flex items-end shrink-0 p-0 z-[1]"
@@ -156,7 +129,7 @@ const SignIn: FunctionComponent = () => {
                       onChange={(event) => setPasswordTextValue(event.target.value)}
                   />
                   <img
-                      className="cursor-pointer absolute top-1/2 right-2 transform -translate-y-1/2 h-[30px] w-[30px]"
+                      className="cursor-pointer absolute top-1/2 right-2 transform -translate-y-1/2 h-[20px] w-[20px]"
                       src="/eye_password.png"
                       alt="toggle password visibility"
                       onClick={togglePasswordVisibility}
@@ -180,6 +153,7 @@ const SignIn: FunctionComponent = () => {
     </div>
   );
 }
+
 
 
 export default SignIn;
