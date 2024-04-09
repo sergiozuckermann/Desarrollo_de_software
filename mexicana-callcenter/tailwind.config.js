1937ca4a64bb3836189926eb3c8527a1ffd22dd5
fernicortlo <a01026613@tec.mx>
2024-04-08 23:10:23 -0600

    adds font links, map and agent overview header.

--- mexicana-callcenter/tailwind.config.js
+++ mexicana-callcenter/tailwind.config.js
@@ -21,6 +21,11 @@ module.exports = {
     },
     fontSize: {
       lg: "18px",
+      "181xl": "200px",
+      "31xl": "50px",
+      "61xl": "80px",
+      base: "16px",
+      xl: "20px",
       "41xl": "60px",
       inherit: "inherit",
     },
@@ -43,3 +48,5 @@ module.exports = {
     preflight: false,
   },
 };
+
+
