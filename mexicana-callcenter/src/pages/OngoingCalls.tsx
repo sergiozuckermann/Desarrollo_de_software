a0c1f110584183910a871424212631c734f9886f
fernicortlo <a01026613@tec.mx>
2024-04-10 18:09:22 -0600

    changes logo on hello page to transparent backgorund one and makes working buttons (notif & home)

--- mexicana-callcenter/src/pages/OngoingCalls.tsx
+++ mexicana-callcenter/src/pages/OngoingCalls.tsx
@@ -27,29 +27,43 @@ const OngoingCalls: FunctionComponent = () => {
       {/* Top bar with background */}
       <div className="flex h-20 bg-tertiary shadow-lg justify-between items-center p-4">
         <div>
+          {/* LA RUTA ESTA A LA PÁGINA DE HELLO POR QUE TODAVÍA NO TENEMOS HOME */}
+        <button 
+          onClick={() => window.location.href = '/'} 
+          className="flex items-center border-none bg-transparent hover-shrink-button"
+        >
           <img 
             src="/logo_callCenter_color.png"  
             alt="" 
             className="w-[230px] ml-3"
           />
+        </button>
         </div>
         <div className="flex items-center">
+          {/* LA RUTA ESTA A UNA PÁGINA VACIA */}
+        <button 
+          onClick={() => window.location.href = '/Notifications'} 
+          className="flex items-center border-none bg-transparent hover-shrink-button"
+        >
           <img 
             src="/notifications_iconn.png" 
             alt="" 
             className="w-[45px] mr-2"
           />
+        </button>
           <h1 className="font">| Map</h1>
         </div>
       </div>
       {/* Main content */}
       <div className="flex h-[90%]">
         <div>
+        
           <img 
             src="/onCallBlurb.png" 
             alt=""
             className="mt-12 ml-12 w-[60px] h-[60px]"
           />
+        
         </div>
         <div className="flex-grow overflow-auto"> {/* Allow scrolling only within the main content if necessary */}
           <h1 className="font1">Agents Overview</h1>
