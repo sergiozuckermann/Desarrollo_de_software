4a70d4a8acec055b695523554a3f36cd98645d64
Santiago Benitez <A01782813@tec.mx>
2024-04-16 19:45:01 -0600

    tested the web app from the server address, ready to test deployment

--- mexicana-callcenter/backend/index.js
+++ mexicana-callcenter/backend/index.js
@@ -21,7 +21,13 @@ app.use(cors())
 app.use(express.json());
 
 // Serve static files from the React production build directory
-app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
+
+// Serve the index.html for any other routes to let React Router handle them
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 // middleware to set the token to the request
 app.use(setToken) 
