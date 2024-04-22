c995298ffdb26cdeea80176711f1def6f307718c
Ian <A01027225@tec.mx>
2024-04-21 23:33:10 -0600

    add Supervisor data

--- mexicana-callcenter/backend/controllers/supervisor.js
+++ mexicana-callcenter/backend/controllers/supervisor.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const supervisorRouter = express.Router();
+const dynamoDBClient = require('../utils/dynamoDBClient')
+const { ScanCommand } = require("@aws-sdk/client-dynamodb"); // CommonJS import
+const { unmarshall } = require('@aws-sdk/util-dynamodb');
 
-supervisorRouter.get('/', (req, res) => {
+supervisorRouter.get('/myInfo/:username', async (req, res, next) => {
+    const username = req.params.username
+
+    // Define parameters for query operation to find information of the user
+    const params = { 
+    TableName: 'Supervisor',
+    FilterExpression: 'username = :u',
+    ExpressionAttributeValues: { // use expression to filter by username
+      ':u': { S: username }
+    }
+  };
+
+    try {
+      const command = new ScanCommand(params);
+      const response = await dynamoDBClient.send(command);
+      console.log("this is response: ", response)
+      const [userInfo] = response.Items // extract the info of the user
+      res.status(200).json(unmarshall(userInfo)) // return info to the client as an object that contains info of the user
+  } catch (err) {
+      console.log("error found: ", err)
+      res.status(500).json({error: "There was a problem finding the user information"})
+
+  }
 })
 
 module.exports = supervisorRouter
--- mexicana-callcenter/frontend/src/pages/SupervisorMain.tsx
+++ mexicana-callcenter/frontend/src/pages/SupervisorMain.tsx
@@ -1,71 +1,37 @@
-// import { useState, useEffect } from 'react';
-// import PageStructure from "../components/PageStructure";
-// import HomeButton from "../components/HomeButtons";
-// import SupervisorButton from "../components/SupervisorButton"; 
-// import WorkerCard from '../components/WorkerCard';
-
-// const handleMetricsButton = () => {
-//   window.location.href = '/Metrics'; 
-// };
-// const handleAgentSpotlightButton = () => {
-//   window.location.href = '/AgentSpotlight'; 
-// };
-// const handleCustomerButton = () => {
-//   window.location.href = '/CustomerService'; 
-// };
-
-// const handleBreaksButton = () => {
-//   window.location.href = '/Breaks'; 
-// };
-// const handleCallingButton = () => {
-//     window.location.href = '/supervisor/onGoingCalls'; 
-//   };
-
-
-// const MainContent = () => {
-
-//   return (
-//     <div className="grid grid-cols-12 gap-1 w-90% h-[90%] inset-y-1/2 items-center content-center ">
-//       <div className="flex flex-col col-span-12 md:col-span-4">
-//         <WorkerCard  name="Ana Gabriel Martínez" position="Agent" years={2} points={200} status="Active" />
-//       </div>
-//       <div className="flex flex-col col-span-12 md:col-span-8">
-//         <div className="flex-1 p-8 border-2 border-gray-400 bg-tertiary border-black-500 home-button rounded-xl">
-//           <div className="grid h-full gap-8 mx-auto md:grid-cols-2">
-//             <HomeButton icon="/MetricsSymbol.svg" title="My Metrics" subtitle="See the real time metrics for all the agents" handleClick={handleMetricsButton}/>
-//             <HomeButton icon="/SpotlightSymbol.svg" title="Agent Spotlight" subtitle="Weekly best agents" handleClick={handleAgentSpotlightButton}/>
-//             <HomeButton icon="/costumer.svg" title="Custumer Service" subtitle="See the metrics for the costumer service surveys" handleClick={handleCustomerButton} />
-//             <HomeButton icon="/BreakSymbol.svg" title="Take a break" subtitle="Go to take a break to clear the mind" handleClick={handleBreaksButton}/>
-//           </div>
-//         </div>
-//         <SupervisorButton
-//            handleClick={handleCallingButton} />  
-//     </div>
-//     </div>
-//   );
-// };
-
-// const SupervisorMain = () => {
-//   return (
-//     <PageStructure title="Home">
-//       <MainContent />
-//     </PageStructure>
-//   );
-// };
-
-// export default SupervisorMain;
-
-
+import { useState, useEffect } from 'react';
 import PageStructure from "../components/PageStructure";
 import HomeButton from "../components/HomeButtons"; 
 import SupervisorButton from "../components/SupervisorButton";
 import WorkerCard from '../components/WorkerCard';
+import axios from 'axios'
+import { useAuth } from '../hooks/useAuth'
+
 
 const MainContent = () => {
+  const [buttonMode, setButtonMode] = useState('workspace');
+  const [userInfo, setUserInfo] = useState<object | null>(null);
+  const { username } = useAuth()
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setButtonMode(prevMode => prevMode === 'workspace' ? 'calling' : 'workspace');
+    }, 10000);
+    return () => clearInterval(timer);
+  }, []);
+
+  // execute call to backend url to fetch info of the user
+  useEffect(() => {
+    // obtain user information
+    axios
+      .get(`http://localhost:3000/Supervisor/myinfo/${username}`)
+      .then((response) => setUserInfo(response.data))
+      .catch((error) => console.log(error))
+
+  }, [])
   return (
     <div className="grid w-full h-full grid-cols-1 gap-4 p-4 md:grid-cols-12">
       <div className="md:col-span-4">
-        <WorkerCard name="Ana Gabriel Martínez" position="Agent" years={2} points={200} status="Active" />
+        { userInfo !== null ? <WorkerCard name={userInfo.name} position={userInfo.position} years={userInfo.experience} points={200} status="Active" /> : null}
       </div>
       <div className="flex flex-col space-y-4 md:col-span-8">
   <div className="flex flex-col gap-10">
