0e190f4ed1e9347781a60a8888805c545846e03f
David Flores Becerril <a01368391@itesm.mx>
2024-04-21 20:33:12 -0600

    Added disclaimer about the credentials

--- mexicana-callcenter/frontend/cypress/e2e/deleteTestUser.ts
+++ mexicana-callcenter/frontend/cypress/e2e/deleteTestUser.ts
@@ -4,8 +4,8 @@ export async function deleteUser(userPoolId: string, username: string) {
     const client = new CognitoIdentityProviderClient({
         region: "us-east-1",
         credentials: {
-            accessKeyId: Cypress.env('accessKeyId'),
-            secretAccessKey: Cypress.env('secretAccessKey'),
+            accessKeyId: Cypress.env('accessKeyId'), //Use your own accessKeyId to delete the user
+            secretAccessKey: Cypress.env('secretAccessKey'), //Use your own secretAccessKey to delete the user
         }
     });
     const command = new AdminDeleteUserCommand({
