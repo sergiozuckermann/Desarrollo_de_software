1e616d2677464f07c8024f8368669ce89df8f1c1
Arantza Parra <90363218+ashaparra@users.noreply.github.com>
2024-04-25 16:33:11 -0600

    Add:Better symbols

--- mexicana-callcenter/frontend/src/components/piechart.tsx
+++ mexicana-callcenter/frontend/src/components/piechart.tsx
@@ -13,40 +13,114 @@ interface PieChartDataItem {
   }
 
   const MyPieChart: React.FC<MyPieChartProps> = ({ data }) => {
-  return (
-    <div style={{ height: 400 }}>
-      <ResponsivePie
+    const brightColors = ['#5BC0EB', '#004BA8', '#4A525A', '#c7f9cc', '#57cc99', '#800080', '#FF4500', '#9ACD32'];
+        return (
+    <ResponsivePie
         data={data}
-        margin={{top:10, right: 80, bottom: 80, left: 80 }}
+        margin={{ top:2, right: 80, bottom: 80, left: 80 }}
         innerRadius={0.5}
         padAngle={0.7}
         cornerRadius={3}
         activeOuterRadiusOffset={8}
-        colors={{ scheme: 'nivo' }}
+        colors={brightColors}
         borderWidth={1}
-        borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+        borderColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    0.2
+                ]
+            ]
+        }}
         arcLinkLabelsSkipAngle={10}
         arcLinkLabelsTextColor="#333333"
         arcLinkLabelsThickness={2}
         arcLinkLabelsColor={{ from: 'color' }}
         arcLabelsSkipAngle={10}
-        arcLabelsTextColor={{ from: 'color', modifiers: [['darker', 2]] }}
-        tooltip={({ datum }) => (
-          <div style={{ color: datum.data.color, padding: '5px 9px', background: '#ffffff', boxShadow: '0 3px 9px rgba(0, 0, 0, 0.5)' }}>
-            <strong>{datum.id}:</strong> {datum.value}
-          </div>
-        )}
-        theme={{
-          tooltip: {
-            container: {
-              background: '#333',
-            },
-          },
+        arcLabelsTextColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    2
+                ]
+            ]
         }}
-      />
-    </div>
-  );
+        // defs={[
+        //     {
+        //         id: 'dots',
+        //         type: 'patternDots',
+        //         background: 'inherit',
+        //         color: 'rgba(255, 255, 255, 0.3)',
+        //         size: 4,
+        //         padding: 1,
+        //         stagger: true
+        //     },
+        //     {
+        //         id: 'lines',
+        //         type: 'patternLines',
+        //         background: 'inherit',
+        //         color: 'rgba(255, 255, 255, 0.3)',
+        //         rotation: -45,
+        //         lineWidth: 6,
+        //         spacing: 10
+        //     }
+        // ]}
+        // fill={[
+        //     {
+        //         match: {
+        //             id: 'ruby'
+        //         },
+        //         id: 'dots'
+        //     },
+        //     {
+        //         match: {
+        //             id: 'c'
+        //         },
+        //         id: 'dots'
+        //     },
+        //     {
+        //         match: {
+        //             id: 'go'
+        //         },
+        //         id: 'dots'
+        //     },
+        //     {
+        //         match: {
+        //             id: 'python'
+        //         },
+        //         id: 'dots'
+        //     },
+        //     {
+        //         match: {
+        //             id: 'scala'
+        //         },
+        //         id: 'lines'
+        //     },
+        //     {
+        //         match: {
+        //             id: 'lisp'
+        //         },
+        //         id: 'lines'
+        //     },
+        //     {
+        //         match: {
+        //             id: 'elixir'
+        //         },
+        //         id: 'lines'
+        //     },
+        //     {
+        //         match: {
+        //             id: 'javascript'
+        //         },
+        //         id: 'lines'
+        //     }
+        // ]}
+        motionConfig="wobbly"
+        legends={[]}
+    />
+    );
 };
-
-
 export default MyPieChart;
+  
