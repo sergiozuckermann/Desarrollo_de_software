80ac1384e3e13bea706751961d502d2aad24ae63
Arantza Parra <90363218+ashaparra@users.noreply.github.com>
2024-04-25 15:01:32 -0600

    Add

--- mexicana-callcenter/frontend/src/pages/AgentMain.tsx
+++ mexicana-callcenter/frontend/src/pages/AgentMain.tsx
@@ -20,17 +20,17 @@ const MainContent = () => {
         <WorkerCard name="Ana Gabriel Martínez" position="Agent" years={2} points={200} status="Active" />
       </div>
       <div className="flex flex-col space-y-4 md:col-span-8">
-  <div className="flex flex-col gap-10">
-    <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
-      <HomeButton icon="/MetricsSymbol.svg" title="My Metrics" subtitle="See the real time metrics for all the agents" handleClick={() => window.location.href = '/Metrics'}/>
-      <HomeButton icon="/SpotlightSymbol.svg" title="Agent Spotlight" subtitle="Weekly best agents" handleClick={() => window.location.href = '/AgentSpotlight'}/>
-      <HomeButton icon="/BadgesSymbol.svg" title="My Badges" subtitle="See all the awards and badges earned" handleClick={() => window.location.href = '/badges'}/>
-      <HomeButton icon="/BreakSymbol.svg" title="Take a break" subtitle="Go to take a break to clear the mind" handleClick={() => window.location.href = '/Breaks'}/>
+        <div className="flex flex-col gap-10">
+          <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
+            <HomeButton icon="/MetricsSymbol.svg" title="My Metrics" subtitle="See the real time metrics for all the agents" handleClick={() => window.location.href = '/Metrics'}/>
+            <HomeButton icon="/SpotlightSymbol.svg" title="Agent Spotlight" subtitle="Weekly best agents" handleClick={() => window.location.href = '/AgentSpotlight'}/>
+            <HomeButton icon="/BadgesSymbol.svg" title="My Badges" subtitle="See all the awards and badges earned" handleClick={() => window.location.href = '/badges'}/>
+            <HomeButton icon="/BreakSymbol.svg" title="Take a break" subtitle="Go to take a break to clear the mind" handleClick={() => window.location.href = '/Breaks'}/>
+          </div>
+            <GradientButton mode={buttonMode} handleClick={() => window.location.href = '/agent/workspace'} />
+        </div>
+      </div>
     </div>
-    <GradientButton mode={buttonMode} handleClick={() => window.location.href = '/agent/workspace'} />
-  </div>
-</div>
-</div>
   );
 };
 
