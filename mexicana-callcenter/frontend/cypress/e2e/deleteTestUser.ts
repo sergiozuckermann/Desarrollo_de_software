bf39430798794bbc3559c706587da69240a17d96
Hedguhar Domínguez <hedguhar2511@gmail.com>
2024-04-29 17:48:11 -0600

    Correction of login and signup tests

--- mexicana-callcenter/frontend/cypress/e2e/deleteTestUser.ts
+++ mexicana-callcenter/frontend/cypress/e2e/deleteTestUser.ts
@@ -1,15 +1,21 @@
 import { CognitoIdentityProviderClient, AdminDeleteUserCommand } from "@aws-sdk/client-cognito-identity-provider";
+import cypress from "cypress";
+
+require('dotenv').config();
 
 export async function deleteUser(userPoolId: string, username: string) {
-    const client = new CognitoIdentityProviderClient({ region: "us-east-1" , credentials: {
-        accessKeyId: Cypress.env('accessKeyId'),
-        secretAccessKey: Cypress.env('secretAccessKey'),
-    }});
+    const client = new CognitoIdentityProviderClient({
+        region: "us-east-1",
+        credentials: {
+            accessKeyId: Cypress.env('accessKeyId'),
+            secretAccessKey: Cypress.env('secretAccessKey'),
+        }
+    });
     const command = new AdminDeleteUserCommand({
         UserPoolId: userPoolId,
         Username: username,
     });
-    
+
     try {
         const response = await client.send(command);
         console.log("User deleted successfully");
