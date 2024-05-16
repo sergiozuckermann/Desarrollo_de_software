ee8151b6607dd3f4060ff0147d4c244cf69278d7
Arantza Parra <90363218+ashaparra@users.noreply.github.com>
2024-05-16 12:35:36 -0600

    Commented help button

--- mexicana-callcenter/frontend/src/pages/Metrics.tsx
+++ mexicana-callcenter/frontend/src/pages/Metrics.tsx
@@ -62,9 +62,9 @@ const MainContent = () => {
                 <div className="flex flex-row justify-between mt-2 group-hover:text-gray-200">
                     <p>Average Queue Answer Time (ASA)</p>
                     <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-indigo-600 group-hover:text-gray-200" viewBox="0 0 20 20" fill="currentColor">
+                        {/* <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-indigo-600 group-hover:text-gray-200" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
-                        </svg>
+                        </svg> */}
                     </span>
                 </div>
             </div>
