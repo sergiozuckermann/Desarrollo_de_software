98ebf0526971e462c6bacc604df994583c77b92c
Mau Tumalan <93782541+mtumalan@users.noreply.github.com>
2024-06-03 19:03:56 -0600

    Responsiveness to QueueTransfer
    -Added responsiveness :)

--- mexicana-callcenter/frontend/src/pages/QueueTransfer.tsx
+++ mexicana-callcenter/frontend/src/pages/QueueTransfer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import Agent from '../components/AgentDrag';
+import Agent from '../components/Agent';
 import CardDrop from '../components/CardDrop';
 import userService from "../services/user";
 import PageStructure from '../components/PageStructure';
@@ -17,15 +17,14 @@ const AgentRoutingProfile = () => {
     routingProfileId: string;
   };
 
-  const [agents, setAgents] = useState<Agent[]>([]); // State to store the agents
-  const [loading, setLoading] = useState(true); // State to store the loading status
-  const [searchName, setSearchName] = useState(''); // State to store the name
-  const [searchLastName, setSearchLastName] = useState(''); // State to store the last name
-  const [searchUsername, setSearchUsername] = useState(''); // State to store the username
-  const [buttonClicked, setButtonClicked] = useState(false); // State to store the button clicked status
+  const [agents, setAgents] = useState<Agent[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [searchName, setSearchName] = useState('');
+  const [searchLastName, setSearchLastName] = useState('');
+  const [searchUsername, setSearchUsername] = useState('');
+  const [buttonClicked, setButtonClicked] = useState(false);
 
-
-  const routingProfilesMap = { // Map of routing profiles
+  const routingProfilesMap = {
     'cef57a3d-e69c-410f-a52a-511cdd89664b': 'Flight Management',
     '0de58771-443e-461c-bb5d-3999dea7dfb6': 'Website Assistance',
     '10700746-2ca6-4e5f-a882-4c274cd1e2b4': 'Customer Care',
@@ -35,9 +34,9 @@ const AgentRoutingProfile = () => {
     'e2ab2ae8-dc73-490e-9147-196ad04c7e87': 'Test Profile',
   };
 
-  const agentRefs = useRef<{ [key: string]: HTMLDivElement | null }>({}); // Create a ref to store the agent elements
+  const agentRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
 
-  const loadAgents = async () => { // Function to load the agents
+  const loadAgents = async () => {
     try {
       const agentsData = await userService.GetAgents();
       setAgents(agentsData);
@@ -47,12 +46,11 @@ const AgentRoutingProfile = () => {
     }
   };
 
-  useEffect(() => { // Effect to update the agentRefs when the agents change
+  useEffect(() => {
     loadAgents();
   }, []);
 
   const handleAgentDrop = async (agentId: string, newRoutingProfileId: string) => {
-
     try {
       const config = {
         headers: {
@@ -64,25 +62,16 @@ const AgentRoutingProfile = () => {
         { userId: agentId, routingProfileId: newRoutingProfileId }, config
       );
 
-      // Update the agents state
       setAgents((prevAgents) => {
-        // Find the moved agent
         const movedAgent = prevAgents.find(agent => agent.id === agentId);
-        if (!movedAgent) return prevAgents; // If the agent is not found, do nothing
+        if (!movedAgent) return prevAgents;
 
-        // Delete the moved agent from the previous routing profile
         const updatedAgents = prevAgents.filter(agent => agent.id !== agentId);
-
-        // Update the routing profile of the moved agent
         movedAgent.routingProfileId = newRoutingProfileId;
-
-        // Add the moved agent to the new routing profile
         return [...updatedAgents, movedAgent];
       });
       NotificationManager.success('Agent queue updated', 'Update Success');
-
     } catch (error) {
-      // console.error('Error al actualizar el perfil de enrutamiento:', error);
       NotificationManager.error('Failed to update the queue', 'Update Error');
     }
   };
@@ -103,11 +92,10 @@ const AgentRoutingProfile = () => {
           agentElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
           agentElement.classList.add('highlight');
   
-          // Remove highlight after 2 seconds
           setTimeout(() => {
             agentElement.classList.remove('highlight');
           }, 2000);
-        }, 100); // Add a small delay to ensure DOM update
+        }, 100);
       }
     } else {
       NotificationManager.error('User not found', 'Search Error');
@@ -118,8 +106,7 @@ const AgentRoutingProfile = () => {
     return <div>Loading...</div>;
   }
 
-  // Group agents by routing profile
-  const agentsByRoutingProfile: {[key: string]: any[]} = agents.reduce((acc, agent) => {
+  const agentsByRoutingProfile: { [key: string]: any[] } = agents.reduce((acc, agent) => {
     const { routingProfileId } = agent;
     if (!acc[routingProfileId]) {
       acc[routingProfileId] = [];
@@ -129,8 +116,8 @@ const AgentRoutingProfile = () => {
   }, {});
 
   return (
-    <div>
-       <InstructionText>
+    <div style={{ height: '100%', overflowY: 'auto' }}>
+      <InstructionText>
         Drag and drop an agent according to the current Contact Center needs.
       </InstructionText>
       <SearchContainer>
