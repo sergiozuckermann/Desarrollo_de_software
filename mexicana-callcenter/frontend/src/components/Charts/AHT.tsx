598478e83e0abfe20df3e1656c135e88d4df1477
Arantza Parra <90363218+ashaparra@users.noreply.github.com>
2024-06-13 18:57:21 -0600

    Aht comments

--- mexicana-callcenter/frontend/src/components/Charts/AHT.tsx
+++ mexicana-callcenter/frontend/src/components/Charts/AHT.tsx
@@ -1,12 +1,16 @@
+// Average Handle Time component
 import React from 'react';
 
+// Importes React Library
 interface AHTProps {
   callDuration: string;
 }
 
+// Defines the AHT component
 const AHT: React.FC<AHTProps> = ({ callDuration }) => {
   return (
-    <div className='w-full h-full z-70 pl-8' style={{ position: 'relative', zIndex:100, paddingBottom:10}}>
+    // Returns a div with the call duration of the current call 
+    <div className='w-full h-full pl-8 z-70' style={{ position: 'relative', zIndex:100, paddingBottom:10}}>
       <p className="mb-12 text-sm dark:text-white">
         <strong>Talk time of current Call:</strong> {callDuration}
       </p>
